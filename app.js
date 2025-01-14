77e62f8aac4448bf6fdc9281c1f64e2cfdb09018
suleyman-shouib <shouibsuleyman@gmail.com>
2025-01-14 15:16:52 -0600

    Suleyman and Nicole completed User Story 1 by adding a search bar that is able to query through the api by videogame title, displaying all videogames matching the name query, and added temporary, minor css for the games list that is displayed by the search

--- app.js
+++ app.js
@@ -2,12 +2,8 @@
 
 angular.module('videogameFinds', [
     'ngRoute',
-<<<<<<< HEAD
     'videogameList',
     'searchBar'
-=======
-    'videogameList'
->>>>>>> bc1ab6ca1c5028b5e7587abaf3260cb8241cd02e
   ])
   .config(function($locationProvider, $routeProvider) {
     $routeProvider
