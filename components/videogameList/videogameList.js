bc1ab6ca1c5028b5e7587abaf3260cb8241cd02e
Nicole Listerfelt <nlisterfelt@gmail.com>
2025-01-14 14:06:26 -0600

    fixed videogameList.js component name to display a list of games

--- components/videogameList/videogameList.js
+++ components/videogameList/videogameList.js
@@ -1,13 +1,11 @@
 angular.module('videogameList', [])
-.component('videogame-list', {
-    templateUrl: 'components/videogameList/videogameList.html',
-    controller: function($scope, $http){
-        console.log("inside component")
-        $http.get('https://www.cheapshark.com/api/1.0/games?title=batman')
-            .then((response) => {
-                $scope.videogames = response.data;
+    .component('videogameList', {
+        templateUrl: 'components/videogameList/videogameList.html',
+        controller: function($scope,$http){
+            $http.get("https://www.cheapshark.com/api/1.0/games?title=batman")
+            .then(response => {
+                $scope.videogames = response.data
                 console.log($scope.videogames)
-                
             })
-    }
-})
+        }
+    })
