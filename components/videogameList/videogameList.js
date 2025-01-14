de0258b1e97721cca582ad5609975d407756bf2a
suleyman-shouib <shouibsuleyman@gmail.com>
2025-01-14 15:09:50 -0600

    trying to commit

--- components/videogameList/videogameList.js
+++ components/videogameList/videogameList.js
@@ -1,18 +1,3 @@
-// angular.module('videogameList', [])
-
-// .component('videogameList', {
-//     templateUrl: 'components/videogameList/videogameList.html',
-//     controller: function($scope, $http, searchBarService){
-//         $scope.videogames = ''
-
-//         $http.get('https://www.cheapshark.com/api/1.0/games?title=batman')
-//             .then((response) => {
-//                 $scope.videogames = response.data;
-//                 console.log($scope.videogames)
-
-//             })
-//     }
-// })
 angular.module('videogameList', [])
 
 .component('videogameList', {
