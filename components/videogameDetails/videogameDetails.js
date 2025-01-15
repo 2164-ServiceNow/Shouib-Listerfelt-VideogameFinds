c9a0369dcaf98f87891bfe2a9163659d3d4ef166
suleyman-shouib <shouibsuleyman@gmail.com>
2025-01-15 14:43:14 -0600

    Suleyman and Nicole created a pop up 'modal' containing videogame details, completing User Story 2

--- components/videogameDetails/videogameDetails.js
+++ components/videogameDetails/videogameDetails.js
@@ -4,12 +4,18 @@ angular.module('videogameDetails', [])
         bindings: {
             videogame: '<'
         },
-        controller: function($scope, $http){
+        controller: function($scope){
             $scope.$watch('$ctrl.videogame', function(newVideogame){
                 $scope.name = newVideogame.gameInfo.name
                 $scope.imageUrl = newVideogame.gameInfo.thumb
                 $scope.salePrice = newVideogame.gameInfo.salePrice
                 $scope.steamRatingText = newVideogame.gameInfo.steamRatingText
+                $scope.showDetails = true;
             })
+
+            $scope.hideInfo = function(){
+                $scope.showDetails = false;
+            }
+
         }
     })
