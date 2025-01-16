d5ba20c66010ee45f5cd1d28c019ec91c492d3e4
Nicole Listerfelt <nlisterfelt@gmail.com>
2025-01-16 14:05:35 -0600

    Suleyman and Nicole fully incorporated localstorage to add and remove videogames from a wishlist. Completing the minimal viable user story 5.

--- components/wishlist/wishlist.js
+++ components/wishlist/wishlist.js
@@ -1,19 +1,23 @@
 angular.module('wishlist', [])
     .component('wishlist', {
         templateUrl: 'components/wishlist/wishlist.html',
-        controller: function($scope){ 
+        controller: function($scope, wishlistRemoveService){ 
 
             $scope.loadWishlist = function(){
-                console.log("Parsing...")
+                $scope.wishlistArray = []
                 for (let key in localStorage) {
-                    let game = JSON.parse(localStorage.getItem(key))
-                    if(game !== null){
-                        
-                        console.log(game.gameInfo);
+                    if(key>0){
+                        let game = JSON.parse(localStorage.getItem(key))
+                        $scope.wishlistArray.push(game)
                     }
-                    
                 }
             }
-            
+            // $scope.selectVideogame = function(videogame){
+            //     $scope.selectedVideogame = videogame
+            // }
+            $scope.callWishlistRemover = function(gameID){
+                wishlistRemoveService.wishlistRemover(gameID)
+                $scope.loadWishlist()
+            }
         }
     })
--- services/wishlistAddService.js
+++ services/wishlistAddService.js
@@ -1,11 +1,8 @@
-localStorage.clear()
 angular.module('videogameFinds')
     .service('wishlistAddService', function(){
         
         this.wishlistAdder = function(gameID, newVideogame){
             localStorage.setItem(gameID, JSON.stringify(newVideogame))
-            
-            console.log("game has been added")
         }
 
     })
